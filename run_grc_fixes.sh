dce0e559149dc27d952dec14d7e3b5d8ee891c11
Himanshu Joshi <hihi.joshi@gmail.com>
2025-07-15 00:21:40 +0000

    Corrected volume paths in Docker run commands for run scripts

--- run_grc_fixes.sh
+++ run_grc_fixes.sh
@@ -10,7 +10,7 @@ echo "Current folder: $(pwd)"
 docker run \
   --rm \
   --name rnacloud_runner \
-  -v $(pwd)/data:/data \
-  -v $(pwd)/temp:/temp \
-  -v $(pwd)/output:/output \
+  -v $(pwd)/data:/app/data \
+  -v $(pwd)/temp:/app/temp \
+  -v $(pwd)/output:/app/output \
   rnacloud_runner -m rnacloud_genome_reference.grc_fixes.assess_grc_fixes -o /output/gene_alt_contigs_mapping_clinically_relevant.tsv
