6fdeabc3ed2bfa053177bd34aac79495b16f28fa
Himanshu Joshi <hihi.joshi@gmail.com>
2025-08-01 23:30:34 +0000

    Updated nextflow cleanup script

--- clean_nextflow_runs.sh
+++ clean_nextflow_runs.sh
@@ -3,6 +3,7 @@
 # dont delete the temp directory, it holds the batches which take a long time to download
 rm -rf .nextflow
 rm -rf work/*
-rm -f output/*.tsv
+rm -f output/*
+rm -f temp/*
 rm -rf .nextflow.log
 rm -rf .nextflow.log.*
